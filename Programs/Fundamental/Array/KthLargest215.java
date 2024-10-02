836c21bd97f4b40edf6ef9c93e49c93e6b1e704e
Ankush Saha <ankushsaha806@gmail.com>
2024-10-03 01:37:43 +0530

    new added

---
+++ Programs/Fundamental/Array/KthLargest215.java
@@ -0,0 +1,21 @@
+package Fundamental.Array;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthLargest215 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2;
+        System.out.println(kthLargestElement(arr,k));
+    }
+    public static int kthLargestElement(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i : nums){
+            pq.add(i);
+        }
+        for(int i = 1 ; i < k ; i++){
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
